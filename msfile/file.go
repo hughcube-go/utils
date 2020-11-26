1da844424bfba2bd245cb49c2f39591b509ed566
hugh.li <hugh.li@foxmail.com>
2020-11-26 17:39:12 +0800

    bug: 内存泄露问题

--- msfile/file.go
+++ msfile/file.go
@@ -40,10 +40,14 @@ func Size(path string) int64 {
 
 	var size int64
 	_ = filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
 		if !info.IsDir() {
 			size += info.Size()
 		}
-		return err
+		return nil
 	})
 	return size
 }
